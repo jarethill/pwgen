0add66abfd1b22f63435b5dcae8e533012337d9a
Jaret Hill <jaret.hill@tuta.io>
2020-12-28 02:40:13 -0600

    Added diceware functionality

--- pwgen.py
+++ pwgen.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 import secrets
+import json
 from os import path
 from string import ascii_letters, digits, punctuation
 
@@ -11,13 +12,16 @@ def main():
     parser = argparse.ArgumentParser(description="Generate random passwords")
 
     # Default Variables
-    WORD_COUNT = 5
-    CHARACTER_COUNT = None
     PASSWORDS_TO_GENERATE = 1
 
     DIR = path.dirname(path.realpath(__file__))
     FILE_NAME = "output.txt"
     SAVE_PATH = path.join(DIR, FILE_NAME)
+
+    WORDS_MIN_LENGTH = 5
+    WORDS_MAX_LENGTH = 15
+    PASSWORD_MIN_LENGTH = 8
+    PASSWORD_MAX_LENGTH = 128
 
     # Arguments
     parser.add_argument(
@@ -40,8 +44,10 @@ def main():
         "--words",
         metavar="amount",
         type=int,
-        default=WORD_COUNT,
-        help=f"number of words to use in password with diceware (Default: {WORD_COUNT})",
+        default=secrets.choice(
+            [number for number in range(WORDS_MIN_LENGTH, WORDS_MAX_LENGTH + 1)]
+        ),
+        help=f"number of words to use in password with diceware",
     )
 
     parser.add_argument(
@@ -49,9 +55,11 @@ def main():
         "--characters",
         metavar="amount",
         type=int,
-        const=CHARACTER_COUNT,
+        default=secrets.choice(
+            [number for number in range(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH + 1)]
+        ),
         nargs="?",
-        help=f"choose a specific number of characters to use (NOT RECOMMENDED)",
+        help=f"number of characters to use in password",
     )
 
     parser.add_argument(
@@ -62,7 +70,7 @@ def main():
         const=SAVE_PATH,
         metavar="path/to/file",
         dest="save_path",
-        help="save password(s) to file (NOT RECOMMENDED)",
+        help="save password(s) to file (not recommended)",
     )
 
     parser.add_argument(
@@ -77,9 +85,15 @@ def main():
     # Parse arguments
     args = parser.parse_args()
 
-    # Convert relative path into usable path if one is passed with -s argument
+    # Convert relative path into a usable absolute path if one is passed with -s argument
     if args.save_path and args.save_path.startswith("./"):
         args.save_path = path.join(DIR, args.save_path[2:])
+
+    # Add vars to args for use in validate_args
+    args.WORDS_MIN_LENGTH = WORDS_MIN_LENGTH
+    args.WORDS_MAX_LENGTH = WORDS_MAX_LENGTH
+    args.PASSWORD_MIN_LENGTH = PASSWORD_MIN_LENGTH
+    args.PASSWORD_MAX_LENGTH = PASSWORD_MAX_LENGTH
 
     # Validate arguments
     validate_args(args)
@@ -93,16 +107,27 @@ def main():
 
 def generate_password(args):
     if args.diceware:
-        print("Generating with diceware...")
+        try:
+            with open("diceware.json") as f:
+                diceware_json = json.load(f)
+        except IOError:
+            print("Missing diceware.json, file is needed to use the diceware algorithm")
+            sys.exit(1)
+
+        password = ""
+
+        for _ in range(args.words):
+            key = get_diceware_key()
+            word = diceware_json[key]
+
+            password += word + " "
+
+        return password.rstrip()
 
     else:
         valid_characters = ascii_letters + digits + punctuation + " "
 
-        if args.characters:
-            password_length = args.characters
-        else:
-            # If no password length is given, choose one randomly between 10 and 20 inclusive
-            password_length = secrets.choice([number for number in range(10, 21)])
+        password_length = args.characters
 
         while True:
             password = "".join(
@@ -152,18 +177,38 @@ def generate_password(args):
         return password
 
 
-def validate_args(args):
-    PASSWORD_MIN_LENGTH = 8
-    PASSWORD_MAX_LENGTH = 128
+def roll_dice():
+    return secrets.choice([number for number in range(1, 7)])
 
+
+# Generates a 5 digit key to be used for the diceware dictionary
+def get_diceware_key():
+    key = ""
+
+    for _ in range(5):
+        number = roll_dice()
+        key += str(number)
+
+    return key
+
+
+# This method is used for extra args validation outside the parser validations
+def validate_args(args):
     # Validate character count
-    if args.characters is not None:
+    if not args.diceware and args.characters is not None:
         if (
-            args.characters < PASSWORD_MIN_LENGTH
-            or args.characters > PASSWORD_MAX_LENGTH
+            args.characters < args.PASSWORD_MIN_LENGTH
+            or args.characters > args.PASSWORD_MAX_LENGTH
         ):
             raise ValueError(
-                f"Character count must be between {PASSWORD_MIN_LENGTH} and {PASSWORD_MAX_LENGTH}"
+                f"Character count must be between {args.PASSWORD_MIN_LENGTH} and {args.PASSWORD_MAX_LENGTH}"
+            )
+
+    # Validate word count
+    if args.diceware and args.words is not None:
+        if args.words < args.WORDS_MIN_LENGTH or args.words > args.WORDS_MAX_LENGTH:
+            raise ValueError(
+                f"Word count must be between {args.WORDS_MIN_LENGTH} and {args.WORDS_MAX_LENGTH}"
             )
 
 
